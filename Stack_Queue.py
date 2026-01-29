49aae4d5d611fe950f79aecf301a82afa6671284
Dev-team2025 <internshipteam.program@gmail.com>
2026-01-29 10:39:07 +0530

    Update Stack_Queue.py
    Updated Queue

--- Stack_Queue.py
+++ Stack_Queue.py
@@ -1,3 +1,36 @@
+class Stack:
+    def __init__(self):
+        self.stack = []
+
+    def push(self, value):
+        self.stack.append(value)
+
+    def pop(self):
+        if self.isempty():
+            return "Stack is empty"
+        return self.stack.pop()
+
+    def isempty(self):
+        return len(self.stack) == 0
+
+    def length(self):
+        return len(self.stack)
+
+    def display(self):
+        print("Stack is ->", self.stack)
+myStack = Stack()
+myStack.push("Iron Man")
+myStack.push("RDJ")
+myStack.push("Yash")
+myStack.display()
+
+myStack.pop()
+
+print(myStack.isempty())
+print(myStack.length())
+
+
+
 class Queue:
     def __init__(self):
         self.queue=[]
@@ -23,4 +56,5 @@ myQueue.dequeue()
 
 print(myQueue.isempty())
 print(myQueue.length())
+
 
